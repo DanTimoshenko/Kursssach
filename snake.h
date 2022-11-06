97008c07c0698afd9a4d68f419a8f412abefce7b
Olha Makovlyeva <107568513+dissomer@users.noreply.github.com>
2022-11-06 15:32:20 +0200

    Update snake.h

--- snake.h
+++ snake.h
@@ -10,20 +10,20 @@
 #include <QRectF>
 #include <QLabel>
 
-class Snake : public QWidget
+class Snake : public QWidget //наслідуємо, щоб можна було відобразити як вікно
 {
     Q_OBJECT
 public:
-    explicit Snake(QWidget *parent = nullptr);
+    explicit Snake(QWidget *parent = nullptr); 
 protected:
    void timerEvent(QTimerEvent*) override;
    void keyPressEvent(QKeyEvent *event) override;
    void paintEvent(QPaintEvent *event) override;
   //void gameOver(QPainter *painter, QString message) override;
 private:
-   static const int DOT_WIDTH = 20;
-   static const int DOT_HEIGHT = 20;
-   static const int FEILD_WIDTH = 20;
+   static const int DOT_WIDTH = 20; //static, так як немає прив'язки до даного об'єкту 
+   static const int DOT_HEIGHT = 20; //условно поля в змійці розбиті на клітинки, тому задаємо ширину і висоту для них (розміри точок)
+   static const int FEILD_WIDTH = 20; //кількість точок
    static const int FEILD_HEIGHT = 20;
    int score = 0;
    int FRAME_WIDTH;
