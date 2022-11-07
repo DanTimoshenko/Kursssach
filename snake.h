e011f9f66d68a895c8ffe181cbdccd559553327e
Taisia <113723880+TaisiaKishynskaya@users.noreply.github.com>
2022-11-07 18:16:24 +0200

    Update snake.h
    Added comments

--- snake.h
+++ snake.h
@@ -17,19 +17,19 @@ public:
     explicit Snake(QWidget *parent = nullptr);
     void setName(QString s);
 protected:
-   void timerEvent(QTimerEvent*) override;
-   void keyPressEvent(QKeyEvent *event) override;
-   void paintEvent(QPaintEvent *event) override;
+   void timerEvent(QTimerEvent*) override;//визивається, якщо відпрацював таймер
+   void keyPressEvent(QKeyEvent *event) override;//при натисканні клавіш
+   void paintEvent(QPaintEvent *event) override;//отрисовка змійки
   //void gameOver(QPainter *painter, QString message) override;
 private:
-   static const int DOT_WIDTH = 20;
-   static const int DOT_HEIGHT = 20;
-   static const int FEILD_WIDTH = 20;
+   static const int DOT_WIDTH = 20;//static, так як немає прив'язки до даного об'єкту
+   static const int DOT_HEIGHT = 20;//условно поля в змійці розбиті на клітинки, тому задаємо ширину і висоту для них (розміри точок)
+   static const int FEILD_WIDTH = 20;//кількість точок
    static const int FEILD_HEIGHT = 20;
    int score = 0;
    int FRAME_WIDTH;
    int FRAME_HEIGHT;
-   static const int DELAY = 150;
+   static const int DELAY = 150;//усі дії виконувати за таймером
    void doDrawing();
    void localApple();
    void move();
@@ -39,18 +39,18 @@ private:
    void setTitle(const QString &text);
    int timerId;
    QString str;
-   QPoint m_apple;
-   enum Directions
+   QPoint m_apple;//зберігається інфа про яблуко
+   enum Directions//усі можливі напрямки руху змійки
    {
        left,
        right,
        up,
        down
    };
-Directions m_dir;
-   bool m_inGame;
-   QVector<QPoint> m_dots;
-   void initGame();
+   Directions m_dir;//запом'ятовує, куди рухається змійка
+   bool m_inGame;//функція, щоб зберігати стан (у грі або ні). локальні змінні починаємо з m, щоб зручно було орієнтуватися у великому коді
+   QVector<QPoint> m_dots;//зберігає інфу з усі точок змійки. Гра двухмірна, тому інфа точок зберігається у двух змінних (за допомогою QPoint, щоб не ускладнювати)
+   void initGame();//ініціалізує гру
    QLabel *score_counter;
 signals:
    void closed(); //Сигнал, который срабатывает, когда пользователь проигрывает и нажимает на Esc
