64c6f768f0554198d84e4964cfbf2a2214b6b689
Taisia <113723880+TaisiaKishynskaya@users.noreply.github.com>
2022-11-07 18:33:54 +0200

    Update snake.cpp
    Added comments

--- snake.cpp
+++ snake.cpp
@@ -6,16 +6,16 @@
 Snake::Snake(QWidget *parent)
     : QWidget{parent}
 {
-    this->resize(DOT_WIDTH * FEILD_WIDTH,DOT_HEIGHT * FEILD_HEIGHT );
-    score_counter = new QLabel("score: 0", this);
+    this->resize(DOT_WIDTH * FEILD_WIDTH,DOT_HEIGHT * FEILD_HEIGHT );// задаємо розміри поля
+    score_counter = new QLabel("score: 0", this);//лічильник ігрового рахунку
     score_counter->move(20, 0);
     FRAME_WIDTH = width()-40;
     FRAME_HEIGHT = height()-40;
     this->setWindowTitle("snake");
-    initGame();
+    initGame();// функція, що ініціалізує гру
 }
 
-void Snake::timerEvent(QTimerEvent *e)
+void Snake::timerEvent(QTimerEvent *e)//спрацьовує кожен раз, після визивання функцій нижче
 {
     if (m_inGame)
     {
@@ -23,13 +23,13 @@ void Snake::timerEvent(QTimerEvent *e)
         move();
         check_field();
     }
-    this->update();
+    this->update();//оновлення даних після перевірок
 }
 
-void Snake::keyPressEvent(QKeyEvent *e)
+void Snake::keyPressEvent(QKeyEvent *e)//відпрацьовує натискання клавіш
 {
-    int key = e->key();
-    if(key==Qt::Key_Left && m_dir != Directions::right){m_dir = Directions::left;}
+    int key = e->key();//потрібно дізнатися, яка подія прийшла від клавіши
+    if(key==Qt::Key_Left && m_dir != Directions::right){m_dir = Directions::left;}//щоб сама на себе не почала повзати перевіряємо кожний напрямок
     if(key==Qt::Key_Right && m_dir != Directions::left){m_dir = Directions::right;}
     if(key==Qt::Key_Up && m_dir != Directions::down){m_dir = Directions::up;}
     if(key==Qt::Key_Down && m_dir != Directions::up){m_dir = Directions::down;}
@@ -45,27 +45,28 @@ void Snake::keyPressEvent(QKeyEvent *e)
 
 void Snake::paintEvent(QPaintEvent *e)
 {
-    Q_UNUSED(e);
-    doDrawing();
+    Q_UNUSED(e);//дана змінна не потрібна
+    doDrawing();//визиваємо функцію для отрисовки
 }
 
-void Snake::doDrawing(){
-    QPainter qp(this);
+void Snake::doDrawing()
+{
+    QPainter qp(this);//за допомогою цього класу виконуєтсья уся отрисовка
     if (m_inGame)
     {
-        qp.drawRect(20, 20, FRAME_WIDTH, FRAME_HEIGHT);
-        qp.setBrush(Qt::yellow);
-        qp.drawEllipse(m_apple.x() * DOT_WIDTH, m_apple.y() * DOT_HEIGHT, DOT_WIDTH, DOT_HEIGHT);
-        for (int i=0; i<m_dots.size();++i)
+        qp.drawRect(20, 20, FRAME_WIDTH, FRAME_HEIGHT);//відрисовує прямокутник
+        qp.setBrush(Qt::yellow);//задаємо колір прямокутника
+        qp.drawEllipse(m_apple.x() * DOT_WIDTH, m_apple.y() * DOT_HEIGHT, DOT_WIDTH, DOT_HEIGHT);//малюємо яблуко
+        for (int i=0; i<m_dots.size(); ++i)//малюємо змійку
         {
-            if(i == 0)
+            if(i == 0)//якщо це голова
             {
-                qp.setBrush(Qt::red);
+                qp.setBrush(Qt::red);//колір та еліпс дял малювання голови
                 qp.drawEllipse(m_dots[i].x() * DOT_WIDTH, m_dots[i].y() * DOT_HEIGHT, DOT_WIDTH, DOT_HEIGHT);
             }
             else
             {
-                qp.setBrush(Qt::green);
+                qp.setBrush(Qt::green);//колір та еліпс для малювання інших частин змійки
                 qp.drawEllipse(m_dots[i].x() * DOT_WIDTH, m_dots[i].y() * DOT_HEIGHT, DOT_WIDTH, DOT_HEIGHT);
             }
         }
@@ -80,41 +81,41 @@ void Snake::doDrawing(){
 
 void Snake::localApple()
 {
-    QTime time= QTime::currentTime();
+    QTime time= QTime::currentTime();//щоб кожен раз значення положення яблука змінювалось рандомно
     srand((uint)time.msec());
 
-    m_apple.rx()= rand() % (DOT_WIDTH-2)+1;
-    m_apple.ry()= rand() % (DOT_HEIGHT-2)+1;
+    m_apple.rx()= rand() % (DOT_WIDTH-2)+1;//щоб ограничити поля, робимо залишок від висоти та ширини клітинок
+    m_apple.ry()= rand() % (DOT_HEIGHT-2)+1;//генерується рандомне положення яблука на ігровому полі
 }
 
-void Snake::move()
+void Snake::move()//рух змійки
 {
-    for (int i = m_dots.size()-1; i>0; --i)
+    for (int i = m_dots.size()-1; i>0; --i)//щоб рухалося уся змійка, а не тільки її голова
     {
-        m_dots[i]=m_dots[i-1];
+        m_dots[i]=m_dots[i-1];//Масив рухає хвіст по предостанньому елементу, усі елементи
     }
-    switch (m_dir){
-    case left: {m_dots[0].rx() -= 1; break;}
-    case right: {m_dots[0].rx() += 1; break;}
-    case up: {m_dots[0].ry() -= 1; break;}
-    case down: {m_dots[0].ry() += 1; break;}
-}
+    switch (m_dir)
+    {
+    case left: {m_dots[0].rx() -= 1; break;}//якщо рухається вліво від нуля, то х щменшується на 1
+    case right: {m_dots[0].rx() += 1; break;}// х збільшується на 1
+    case up: {m_dots[0].ry() -= 1; break;}//у зменшується на 1
+    case down: {m_dots[0].ry() += 1; break;}//у збільшується на 1
+    }
 }
 
-void Snake::check_field()
+void Snake::check_field()//перевіряє чи не вийшла за поля, або не врізалась сама у себе
 {
-    if(m_dots.size()>4)
+    if(m_dots.size()>4)//якщо б точок = або < 4, то врізатися в себе неможливо, тому перевіряємо більше
    {
-
-        for (int i=1; i<m_dots.size(); ++i)
+        for (int i=1; i<m_dots.size(); ++i)//пропускаємо 0, бо голову з головой можна не порівнювати
         {
-            if(m_dots[0] == m_dots[i])
+            if(m_dots[0] == m_dots[i])//голова врізалась у будь-яку точку тіла, поразка
             {
                m_inGame= false;
             }
         }
     }
-    if(m_dots[0].x() >= FEILD_WIDTH-1)
+    if(m_dots[0].x() >= FEILD_WIDTH-1)//перевірка виходу за поля
     {
         m_inGame=false;
     }
@@ -130,13 +131,13 @@ void Snake::check_field()
     {
         m_inGame=false;
     }
-    if(!m_inGame)
+    if(!m_inGame)//якщо ми не у грі, то таймер перестає працьувати
     {
         killTimer(timerId);
     }
 }
 
-void Snake::gameOver(QPainter *painter, QString message)
+void Snake::gameOver(QPainter *painter, QString message)//гра закінчена
 {
     QFont font("Courier", 15, QFont::DemiBold);
     QFontMetrics fm(font);
@@ -158,16 +159,16 @@ void Snake::gameOver(QPainter *painter, QString message)
     initGame();*/
 }
 
-void Snake::checkApple()
+void Snake::checkApple()//перевіяємо чи з'їла яблуко
 {
-    if (m_apple== m_dots[0])
+    if (m_apple== m_dots[0])//координата яблука збігається з координатою голови
     {
-        m_dots.push_back(QPoint(0,0));
-        localApple();
+        m_dots.push_back(QPoint(0,0));//змійка збільшується на одну клітинку в тілі
+        localApple();//яблуко знов рандомно з'являється на полі
         //QMessageBox msgr;
         //msgr.setText("вы получили очко!");
         //msgr.exec();
-        score++;
+        score++;//кількість ігрових балів збільшується на один
         score_counter->setText(tr("score: %1").arg(score));
     }
 }
@@ -178,14 +179,15 @@ void Snake::setName(QString s)
     str = "Game over, " + str; // Перестраиваем его под сообщение проигрыша
 }
 
-void Snake::initGame(){
-    m_inGame=true;
-    m_dots.resize(3);
-    for(int i=0; i<m_dots.size();++i){
-        m_dots[i].rx()=10;//m_dots.size()-i;
-        m_dots[i].ry()=10;
+void Snake::initGame()
+{
+    m_inGame=true;//якщо в ігрі, то true
+    m_dots.resize(3);//спочатку у змійці буде усього три частини (клітинки)
+    for(int i=0; i<m_dots.size();++i)//заповнюємо початкове положення змійки. У нулі знаходиться голова змійки
+    {
+        m_dots[i].rx()=10;//m_dots.size()-i; //rx повертає вказівних на х
+        m_dots[i].ry()=10;//rу повертає вказівних на х. таким чином вказуємо початкові координати
     }
-   localApple();
-   timerId = startTimer(DELAY);
-
+   localApple();//положення яблука в рандомному місті поля
+   timerId = startTimer(DELAY);//по ініціалізації гри запускаєтсья таймер. Через кожен деякий період будуе визивати функцію обробчика
 }
